6ee4ee98b30fda38f7c6d90966b6601d4eb0ef28
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-12-04 17:37:17 +0100

    Removed copied code which wasn't used

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -1,15 +1,7 @@
 function slideEffect(){
-    function toggleButton(toggle){
-        button.style.pointerEvents = toggle;
-    }
     const animationSlider = document.querySelector('.animationSlider');
-    const button = document.querySelector('.header__btn');
-    // Block buttos until animation is finished
-    toggleButton('none');
     animationSlider.classList.add('loadingScreen');
     setTimeout(function(){
-        // Unblock buttons
-        toggleButton('');
         animationSlider.classList.remove('loadingScreen');
     }, 1400);
 };
